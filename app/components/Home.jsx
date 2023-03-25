70d65c31d0db173e8af233361dfde115ad7a33ce
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-25 14:54:41 +0530

    pagination options

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,16 +2,25 @@
 
 import React from 'react'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation';
 import DataInput from './DataInput'
 import Chart from './Chart'
 import SelectParams from './SelectParams'
 const Home = () => {
 
+  const router = useRouter()
+
     const [jsonFileData,setJsonFileData] = useState([])
     const [selectedAttributes,setSelectedAttributes] = useState([])
 
   return (
-    <div className='p-8 flex flex-col gap-4'>
+    <div className='p-8 flex flex-col gap-4 items-center'>
+
+      <h1 className='text-3xl font-bold'>Visualize your data</h1>
+
+      <button className='py-4 px-6 bg-green-600 text-white rounded' onClick={() => {
+        router.push('/datainput')
+      }}>Enter yout Data</button>
      
       <DataInput setJsonFileData={setJsonFileData} />
 
