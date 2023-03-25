63e896fc27119f985823fe4e77d4c2335fd711c4
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-25 10:48:12 +0530

    basic datainput component

--- app/components/DataInput.jsx
+++ app/components/DataInput.jsx
@@ -2,7 +2,10 @@
 const DataInput = () => {
   return (
     <div>
-        
+        <h1>Input Data File (CSV)</h1>
+        <input type="file" id="file" accept=".csv" />
+
+        <button>Crunch🚀</button>
     </div>
   )
 }
