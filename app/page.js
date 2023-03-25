8975e8de7955ef664073920ce4a528084cc1859c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-25 12:22:38 +0530

    cleared page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-// const inter = Inter({ subsets: ['latin'] })
 import Home from './components/Home'
 
 
 export default function App() {
   return (
-    <main className={styles.main}>
+    <main>
         <Home/> 
     </main>
   )
