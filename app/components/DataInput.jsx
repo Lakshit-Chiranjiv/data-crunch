da6b74506f9c344ef2ca15bda201bc561c96cf79
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-25 10:51:04 +0530

    data input styles

--- app/components/DataInput.jsx
+++ app/components/DataInput.jsx
@@ -1,11 +1,11 @@
 
 const DataInput = () => {
   return (
-    <div>
-        <h1>Input Data File (CSV)</h1>
-        <input type="file" id="file" accept=".csv" />
-
-        <button>Crunch🚀</button>
+    <div className="flex flex-col gap-4">
+        <h1 className="text-3xl">Input Data File (CSV)</h1>
+        <input type="file" id="file" accept=".csv" className="bg-blue-400 p-4 rounded"/>
+        <br />
+        <button className="bg-green-400 rounded px-4 py-2">Crunch🚀</button>
     </div>
   )
 }
